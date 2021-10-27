82f7a9a23773db5c6aa222982d163147fd31cedd
Thales Neves <thales.neves@prs.com.br>
2021-10-26 21:22:09 -0300

    change save file path

--- webscraping.py
+++ webscraping.py
@@ -22,7 +22,7 @@ try:
                   'Margem EBIT', 'EV/EBIT', 'Volume Financ.(R$)', 'Market Cap(R$)']
     df = Filtros().aplicar_filtros(df)
    
-    df.to_excel(excel_writer='C:\\Users\\HOME\\OneDrive\\Finanças\\Rancking_acoes_brasileiras.xlsx',
+    df.to_excel(excel_writer='\\Rancking_acoes_brasileiras.xlsx',
                 sheet_name='Rancking de açoes Brasileiras')
 
 finally:
